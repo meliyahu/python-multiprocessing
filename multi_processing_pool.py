b87316c539e298d942a443d54571994957ca138e
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2019-04-24 14:44:24 +0930

    Feat:Demo multi processor vs single

---
+++ multi_processing_pool.py
@@ -0,0 +1,56 @@
+import time
+from datetime import datetime, timedelta
+import os
+from multiprocessing import Pool
+
+
+def sum_square(number):
+    s = 0
+    for i in range(number):
+        s += i * i
+    return s
+
+
+def sum_square_with_mp(numbers):
+    """
+    Demonstrates multi-processing
+    :param numbers:
+    :return:
+    """
+    start_time = time.time()
+    p = Pool(os.cpu_count())  # Distribute according to number of cores
+    result = p.map(sum_square, numbers)
+    p.close()
+    p.join()
+    end_time = time.time() - start_time
+    d = get_formatted_time(end_time)
+    print(f"Processing {len(numbers)} numbers took {d.day - 1} days:{d.hour} hours {d.minute} minute {d.second} seconds WITH multiprocessing")
+
+
+def sum_square_without_mp(numbers):
+    """
+    Demonstrate without multi-processing
+    :param numbers:
+    :return:
+    """
+    start_time = time.time()
+    result = []
+    for number in numbers:
+        result.append(sum_square(number))
+
+    end_time = time.time() - start_time
+    d = get_formatted_time(end_time)
+
+    print(f"Processing {len(numbers)} numbers took {d.day-1} days:{d.hour} hours {d.minute} minute {d.second} seconds WITHOUT multiprocessing")
+
+
+def get_formatted_time(seconds):
+    sec = timedelta(seconds=int(seconds))
+    d = datetime(1, 1, 1) + sec
+    return d
+
+
+if __name__ == "__main__":
+    numbers = range(100000)
+    sum_square_with_mp(numbers)
+    sum_square_without_mp(numbers)

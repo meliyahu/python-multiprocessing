65c6112212682f620a70c4bb0a2a82acee8c4502
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2019-04-24 15:16:41 +0930

    Feat: No locks

--- multi_processing_locks.py
+++ multi_processing_locks.py
@@ -2,24 +2,26 @@ from multiprocessing import Process, Lock, Value
 import time
 
 
-def add_500_no_mp(total):
+def add_500_no_lock(total):
     for i in range(100):
         time.sleep(0.01)
-        total += 5
-    return total
+        total.value += 5
 
 
-def sub_500_no_mp(total):
+def sub_500_no_lock(total):
     for i in range(100):
         time.sleep(0.01)
-        total -= 5
-    return total
+        total.value -= 5
 
 
 if __name__ == '__main__':
-    total = 500
-    print(total)
-    total = add_500_no_mp(total)
-    print(total)
-    total = sub_500_no_mp(total)
-    print(total)
+    total = Value('i', 500)
+    add_process = Process(target=add_500_no_lock, args=(total, ))
+    sub_process = Process(target=sub_500_no_lock, args=(total, ))
+
+    add_process.start()
+    sub_process.start()
+
+    add_process.join()
+    sub_process.join()
+    print(total.value)
